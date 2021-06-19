2124defc38384eae359d2364dca2609ac4b2c7b2
Fitti <dev@fitti.io>
2021-06-19 08:13:50 +0200

    Bump LNbits version

--- install_lnbits.sh
+++ install_lnbits.sh
@@ -3,7 +3,7 @@
 git clone https://github.com/lnbits/lnbits.git
 cd lnbits/
 # This is a commit I know to be working correctly
-git checkout "1e0367a451ee34f380ce8ad22395690b3826a0c8"
+git checkout "cfbd6fca3ae89fdc89e3c2c857dc731b9b600aa3"
 python3 -m venv venv
 ./venv/bin/pip install -r requirements.txt
 cp .env.example .env
