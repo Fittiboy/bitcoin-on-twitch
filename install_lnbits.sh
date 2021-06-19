95b50e402b1f610a5b294c4ee289503e9e0e6b21
Fitti <dev@fitti.io>
2021-06-19 03:40:07 +0200

    Add comments to install script

--- install_lnbits.sh
+++ install_lnbits.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
+# This is a slightly modified version of the install instructions at https://lnbits.org/guide/installation.html
 git clone https://github.com/lnbits/lnbits.git
 cd lnbits/
+# This is a commit I know to be working correctly
 git checkout "1e0367a451ee34f380ce8ad22395690b3826a0c8"
-# ensure you have virtualenv installed, on debian/ubuntu 'apt install python3-venv' should work
 python3 -m venv venv
 ./venv/bin/pip install -r requirements.txt
 cp .env.example .env
 mkdir data
 ./venv/bin/quart assets
 ./venv/bin/quart migrate
+# Run this to catch potential issues in stdout
 ./venv/bin/hypercorn -k trio --bind 0.0.0.0:5000 'lnbits.app:create_app()'
