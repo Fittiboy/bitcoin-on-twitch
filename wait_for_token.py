eea272af999eb0f87ecc772ec55663325e99be6c
Fitti <dev@fitti.io>
2021-06-19 06:18:39 +0200

    Complete Streamlabs authorization flow

--- wait_for_token.py
+++ wait_for_token.py
@@ -1,5 +1,6 @@
 import json
 
+from subprocess import run
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -9,11 +10,25 @@ app = Flask(__name__)
 def get_token():
     """Captures the API token and saves it in settings.json"""
     # The API token is passed in the querystring as 'code'
-    token = request.args.get('code')
+    code = request.args.get('code')
     with open('settings.json') as settings_file:
         settings = json.load(settings_file)
-        settings['access_token'] = token
+        client_id = settings['client_id']
+        client_secret = settings['client_secret']
+        ip = settings['ip']
+    redirect_uri = f"http://{ip}:6969"
+    url = "https://streamlabs.com/api/v1.0/token"
+    d = "grant_type=authorization_code" \
+        f"&code={code}" \
+        f"&client_id={client_id}" \
+        f"&client_secret={client_secret}" \
+        f"&redirect_uri={redirect_uri}"
+    curl = f"curl --request POST --url {url} -d \"{d}\""
+    response = json.loads(run(curl, shell=True, capture_output=True).stdout)
+    print(response)
+    token = response['access_token']
     with open('settings.json', 'w') as settings_file:
+        settings['access_token'] = token
         json.dump(settings, settings_file, indent=4)
 
     return "Token added to settings.json! You should stop the server now!", 200
