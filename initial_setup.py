a5604e5e1fa4dfa29a99c10336224de67ad368ae
Fitti <dev@fitti.io>
2021-06-19 06:00:12 +0200

    Add Lntxbot .env file settings to initial_setup.py

--- initial_setup.py
+++ initial_setup.py
@@ -21,4 +21,18 @@ if __name__ == "__main__":
         json.dump(settings, settings_file, indent=4)
     print("Now installing LNbits...\n")
     run('. ./install_lnbits.sh', shell=True)
+    # Set the .env file for LNbits to use LNPay
+    with open("./lnbits/.env") as env_file:
+        env = {}
+        for line in env_file.readlines():
+            if "=" in line:
+                key, value = line.split("=")
+                env[key] = value
+    api_key = input("\nPlease input the lntxbot API key: ")
+    env['LNTXBOT_KEY'] = f'"{api_key}"\n'
+    env['LNBITS_BACKEND_WALLET_CLASS'] = "LntxbotWallet\n"
+    with open("./lnbits/.env", "w") as env_file:
+        for key, value in env.items():
+            env_file.write(f'{key}={value}')
+
     print("\nAll done! Please continue to the next step in the guide!")
