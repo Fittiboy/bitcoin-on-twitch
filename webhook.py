38685f6940b7f698c6b19734791d101d6f35ccd9
Fitti <dev@fitti.io>
2021-06-19 04:18:06 +0200

    Finalize webhook

--- webhook.py
+++ webhook.py
@@ -1,13 +1,18 @@
 import json
 
+from subprocess import run
 from flask import Flask, request, abort
 from pycoingecko import CoinGeckoAPI
+from waitress import serve
 
 app = Flask(__name__)
 
-# This is the access token for the Streamlabs API
-with open("access_token.json") as at_file:
-    access_token = json.load(at_file)["access_token"]
+with open("settings.json") as settings_file:
+    settings = json.load(settings_file)
+    # This is the access token for the Streamlabs API
+    access_token = settings["access_token"]
+    # This is the currency that is converted to for Streamlabs
+    fiat = settings["fiat"]
 
 
 def convert_to_fiat(sats: int, fiat: str) -> float:
@@ -26,6 +31,38 @@ def webhook():
     """Handles incoming LNbits payments and pushes donations
     to the Streamlabs API"""
     if request.method == 'POST':
-        pass
+        data = request.get_json(force=True)
+        # For logging and debugging, print incoming requests
+        print(request, '\n', data)
+        # The two LNbits extensions used return data in
+        # different formats.  This try-except handles both.
+        try:
+            sats = int(data['amount'] / 1000)
+            comment = data['comment']
+        except KeyError:
+            sats = int(data['amount'])
+            comment = data['description']
+        if not comment:
+            comment = "No message!"
+        amount = convert_to_fiat(sats, 'usd')
+        url = "https://streamlabs.com/api/v1.0/donations"
+        d = "name=bitcoin" \
+            f"&message={str(sats)} sats: {comment}" \
+            f"&identifier=bitcoin_donos" \
+            f"&amount={amount}" \
+            f"&currency={fiat.upper()}" \
+            f"&access_token={access_token}"
+        # For some reason, I had issues making a valid request
+        # with Python's requests library. Please feel free to
+        # fix this. For now, curl works!
+        curl = f"curl --request POST --url {url} -d \"{d}\""
+        response = run(curl, shell=True, capture_output=True)
+        # For logging/debugging purposes
+        print(curl)
+        print(response.stdout)
     else:
         abort(400)
+
+
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port=5000)
