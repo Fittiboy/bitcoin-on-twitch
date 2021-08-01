d83df8d263163fb2cb63e70c10d82eff7c9d2cc7
Fitti <dev@fitti.io>
2021-08-01 07:18:13 +0200

    Remove old stuff

--- initial_setup.py
+++ initial_setup.py
@@ -3,30 +3,6 @@ import json
 from subprocess import run
 
 if __name__ == "__main__":
-    # List of currency codes supported by both
-    # Streamlabs' and CoinGecko's APIs
-    supported_currencies = [
-        "aud", "brl", "cad", "czk", "dkk", "eur",
-        "hkd", "ils", "inr", "jpy", "myr", "mxn",
-        "nok", "nzd", "php", "pln", "gbp", "rub",
-        "sgd", "sek", "chf", "thb", "try", "usd",
-    ]
-    input_string = "Enter the three-letter code of the currency you use " \
-                   "for Streamlabs donations: "
-    while (fiat := input(input_string).lower()) not in supported_currencies:
-        input_string = "Your currency is either not supported, or " \
-                       "you had a typo. Please try again: "
-    client_id = input("\nPlease input your Streamlabs Client ID: ")
-    client_secret = input("\nPlease input your Streamlabs Client Secret: ")
-    ip = input("\nPlease input the IP address of your server: ")
-    with open('settings.json', 'w') as settings_file:
-        settings = {
-            "fiat": fiat,
-            "client_id": client_id,
-            "client_secret": client_secret,
-            "ip": ip,
-        }
-        json.dump(settings, settings_file, indent=4)
     print("Now installing LNbits...\n")
     run('. ./install_lnbits.sh', shell=True)
     # Set the .env file for LNbits to use LNPay
