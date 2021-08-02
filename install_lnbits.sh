5738da82da390742e9afbec5a821a3be16dd7005
Fitti <dev@fitti.io>
2021-08-02 19:46:24 +0200

    Use latest LNbits commit

--- install_lnbits.sh
+++ install_lnbits.sh
@@ -2,8 +2,9 @@
 # This is a slightly modified version of the install instructions at https://lnbits.org/guide/installation.html
 git clone https://github.com/lnbits/lnbits.git
 cd lnbits/
+# going with latest commit for now
 # This is a commit I know to be working correctly
-git checkout "83137ba0a0cc541608482cc43182e26009f63814"
+# git checkout "83137ba0a0cc541608482cc43182e26009f63814"
 python3 -m venv venv
 ./venv/bin/pip install -r requirements.txt
 cp .env.example .env
